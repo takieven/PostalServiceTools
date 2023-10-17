f0a03e02b2b492c2d79658a112ec6816b28bdcff
takieven <mirandastevendave1@gmail.com>
2023-10-17 20:09:49 +0800

    Refactored screens

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,18 +1,18 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountBox
-import androidx.compose.material.icons.filled.CheckCircle
 import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Warning
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import ui.screens.Currency
+import ui.screens.Home
 
 @Composable
 @Preview
@@ -40,69 +40,6 @@ fun App() {
             }
         }
     }
-}
-
-@Composable
-fun Home() {
-    var input by remember { mutableStateOf(Utils.generateSerial(11)) }
-    var valid by remember { mutableStateOf(checkDigit(input)) }
-
-    Box(modifier = Modifier.fillMaxSize().offset(0.dp, 0.dp), contentAlignment = Alignment.Center) {
-        Column(horizontalAlignment = Alignment.CenterHorizontally) {
-            Text("Validate a Serial Number", style = MaterialTheme.typography.h4, color = MaterialTheme.colors.secondaryVariant)
-            Spacer(Modifier.height(15.dp))
-            Row(verticalAlignment = Alignment.CenterVertically) {
-                TextField(
-                    value = input,
-                    onValueChange = {
-                        input = it
-                        valid = checkDigit(input)
-                    },
-                    placeholder = { Text("Serial number") },
-                    isError = !valid,
-                    trailingIcon = {
-                        when (valid) {
-                            true -> Icon(imageVector = Icons.Default.CheckCircle, "check", tint = MaterialTheme.colors.primary)
-                            false -> Icon(imageVector = Icons.Default.Warning, "check")
-                        }
-                    }
-                )
-            }
-
-            Spacer(Modifier.height(3.dp))
-
-            Row {
-                Button(onClick = {
-                    input = Utils.generateSerial(11)
-                    valid = checkDigit(input)
-                },
-                   enabled = when (!valid) {false->true; true->false}
-                    ) {
-                    Text("Generate valid serial")
-                }
-
-                Spacer(Modifier.width(10.dp))
-
-                Button(onClick = {
-                    input = Utils.generateCheckDigit(serial = input)
-                    valid = checkDigit(input)
-                },
-                    enabled = when (valid) {false->true; true->false}
-                    ) {
-                    Text("Generate check digit")
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Currency() {
-    Box(Modifier.fillMaxSize()) {}
-}
-
-fun checkDigit(serial: String): Boolean {
-    return Utils.checkDigit(serial)
 }
 
 fun main() = application {
---
+++ src/main/kotlin/ui/screens/Currency.kt
@@ -0,0 +1,11 @@
+package ui.screens
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun Currency() {
+    Box(Modifier.fillMaxSize()) {}
+}
