f2daf09ba0a07ec07c474cabbb7f3be9d4e04d36
takieven <mirandastevendave1@gmail.com>
2023-10-17 21:50:54 +0800

    Added solution viewer

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,18 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.material.icons.filled.Home
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import ui.screens.Currency
@@ -20,25 +24,28 @@ fun App() {
     var screen by remember { mutableStateOf(SCREENS.HOME) }
 
     MaterialTheme {
-        Row(verticalAlignment = Alignment.CenterVertically) {
-            Column {
-                IconButton(onClick = {
-                    screen = SCREENS.HOME
-                }) {
-                    Icon(imageVector = Icons.Default.Home, "home")
+            Row(
+                Modifier.background(MaterialTheme.colorScheme.surfaceColorAtElevation(4.dp)),
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                Column {
+                    IconButton(onClick = {
+                        screen = SCREENS.HOME
+                    }) {
+                        Icon(imageVector = Icons.Default.Home, "home")
+                    }
+                    IconButton(onClick = {
+                        screen = SCREENS.CURRENCY
+                    }) {
+                        Icon(imageVector = Icons.Default.AccountBox, "home")
+                    }
                 }
-                IconButton(onClick = {
-                    screen = SCREENS.CURRENCY
-                }) {
-                    Icon(imageVector = Icons.Default.AccountBox, "home")
-                }
-            }
 
-            when (screen) {
-                SCREENS.HOME -> Home()
-                SCREENS.CURRENCY -> Currency()
+                when (screen) {
+                    SCREENS.HOME -> Home()
+                    SCREENS.CURRENCY -> Currency()
+                }
             }
-        }
     }
 }
 
--- src/main/kotlin/ui/screens/Home.kt
+++ src/main/kotlin/ui/screens/Home.kt
@@ -1,65 +1,111 @@
 package ui.screens
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.BottomSheetScaffold
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.CheckCircle
 import androidx.compose.material.icons.filled.Warning
+import androidx.compose.material.rememberBottomSheetScaffoldState
+import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
 
+@OptIn(ExperimentalMaterialApi::class, ExperimentalMaterial3Api::class)
 @Composable
 fun Home() {
     var input by remember { mutableStateOf(Utils.generateSerial(11)) }
     var valid by remember { mutableStateOf(Utils.checkDigit(input)) }
 
-    Box(modifier = Modifier.fillMaxSize().offset(0.dp, 0.dp), contentAlignment = Alignment.Center) {
-        Column(horizontalAlignment = Alignment.CenterHorizontally) {
-            Text("Validate a Serial Number", style = MaterialTheme.typography.h4, color = MaterialTheme.colors.secondaryVariant)
-            Spacer(Modifier.height(15.dp))
-            Row(verticalAlignment = Alignment.CenterVertically) {
-                TextField(
-                    value = input,
-                    onValueChange = {
-                        input = it
-                        valid = Utils.checkDigit(input)
-                    },
-                    placeholder = { Text("Serial number") },
-                    isError = !valid,
-                    trailingIcon = {
-                        when (valid) {
-                            true -> Icon(imageVector = Icons.Default.CheckCircle, "check", tint = MaterialTheme.colors.primary)
-                            false -> Icon(imageVector = Icons.Default.Warning, "check")
+    val bottomSheetScaffoldState = rememberBottomSheetScaffoldState()
+
+    BottomSheetScaffold(
+        modifier = Modifier.clip(RoundedCornerShape(16.dp)),
+        backgroundColor = MaterialTheme.colorScheme.surface,
+        sheetShape = RoundedCornerShape(16.dp),
+        sheetContent = {
+            Details(input)
+        },
+        scaffoldState = bottomSheetScaffoldState
+    ) {
+        Box(modifier = Modifier.fillMaxSize()) {
+            Column(Modifier.align(Alignment.Center), horizontalAlignment = Alignment.CenterHorizontally) {
+                Text("Validate a Serial Number", style = MaterialTheme.typography.headlineMedium, color = MaterialTheme.colorScheme.primary)
+                Spacer(Modifier.height(15.dp))
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    TextField(
+                        value = input,
+                        onValueChange = {
+                            input = it
+                            valid = Utils.checkDigit(input)
+                        },
+                        placeholder = { Text("Serial number") },
+                        isError = !valid,
+                        trailingIcon = {
+                            when (valid) {
+                                true -> Icon(imageVector = Icons.Default.CheckCircle, "check", tint = MaterialTheme.colorScheme.primary)
+                                false -> Icon(imageVector = Icons.Default.Warning, "check")
+                            }
                         }
-                    }
-                )
-            }
-
-            Spacer(Modifier.height(3.dp))
-
-            Row {
-                Button(onClick = {
-                    input = Utils.generateSerial(11)
-                    valid = Utils.checkDigit(input)
-                },
-                    enabled = when (!valid) {false->true; true->false}
-                ) {
-                    Text("Generate valid serial")
+                    )
                 }
 
-                Spacer(Modifier.width(10.dp))
+                Spacer(Modifier.height(3.dp))
 
-                Button(onClick = {
-                    input = Utils.generateCheckDigit(serial = input)
-                    valid = Utils.checkDigit(input)
-                },
-                    enabled = when (valid) {false->true; true->false}
-                ) {
-                    Text("Generate check digit")
+                Row {
+                    Button(onClick = {
+                        input = Utils.generateSerial(11)
+                        valid = Utils.checkDigit(input)
+                    },
+                        enabled = when (!valid) {false->true; true->false}
+                    ) {
+                        Text("Generate valid serial")
+                    }
+
+                    Spacer(Modifier.width(10.dp))
+
+                    Button(onClick = {
+                        input = Utils.generateCheckDigit(serial = input)
+                        valid = Utils.checkDigit(input)
+                    },
+                        enabled = when (valid) {false->true; true->false}
+                    ) {
+                        Text("Generate check digit")
+                    }
                 }
             }
         }
     }
+}
+
+@Composable
+fun Details(serial: String) {
+    Column(Modifier.padding(10.dp)) {
+        Header("Solution")
+        Text("The United States Postal Service (USPS) uses 11-digit serial numbers on its money orders. The first ten digits identify the document, and the last digit is the check digit")
+        Divider(Modifier.padding(0.dp, 10.dp))
+        Text("The money order has serial number a = $serial.The money order is identified by the first 10 digits ${serial.removeSuffix(serial[serial.lastIndex].toString())}. The 11th digit ${serial[serial.lastIndex]} is the check digit.")
+        val solution = StringBuilder()
+        serial.forEachIndexed { i, digit ->
+            if (i<serial.lastIndex) {
+                solution.append(digit)
+                if (i < serial.lastIndex-1) {
+                    solution.append(" + ")
+                }
+            }
+        }
+        val sum = serial.toList().map { it.digitToInt() }.take(10).sum()
+        solution.appendLine(" = $sum")
+        solution.append("$sum mod 9 = ${sum%9}")
+        Text(solution.toString())
+    }
+}
+
+@Composable
+fun Header(text: String) {
+    Text(text = text, style = MaterialTheme.typography.headlineMedium, color = MaterialTheme.colorScheme.primary)
 }
